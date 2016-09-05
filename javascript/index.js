3a39ef31b98e1846b28e097d99fac829a100f01e
XboxYan <yanwenbin1991@live.com>
2016-09-05 11:18:48 +0800

    522

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
 $(window).on('scroll',function(ev){
     if(document.body.scrollTop>240){
         $('#nav').addClass('sub');
+        console.log(33)
     }else{
         $('#nav').removeClass('sub');
     }
